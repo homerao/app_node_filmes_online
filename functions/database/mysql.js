7a2452c72aee2739958f14b42783e8402483b038
Homero Tiago Galvão <homerao@gmail.com>
2020-12-25 19:57:08 -0300

    deletando o mysql

--- functions/database/mysql.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize')
-const config = require('../config/config.json')
-
- 
-
-module.exports  = new Sequelize(config.development.database,config.development.username,config.development.password,{host:"mysql-8.c2qdj1boddoe.us-east-2.rds.amazonaws.com", dialect:"mysql"})
-    
