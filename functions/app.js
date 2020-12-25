1e000b1c10f8c32c2cb667ddf6efb44e568f9746
Homero <homerao@gmail.com>
2020-12-25 15:14:32 -0300

    Adicionando a camada de serviço

--- functions/app.js
+++ functions/app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const router = require('express').Router()
 const bodyParser = require('body-parser')
 const handlebars = require('express-handlebars')
+const apiIndex = require('./routes/apiroutes/ApiIndexRoutes')
+const helmet = require('helmet')
 const app = express()
 
 const port = 3000
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(helmet())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.engine('hbs', handlebars({defaultLayout:'main'}))
 app.set('view engine', 'handlebars')
-
+app.use('/', apiIndex)
 
 
 
