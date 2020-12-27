489ada81c47e9c91b84cc7e5ca86a34d3db93626
Homero <homerao@gmail.com>
2020-12-27 14:18:00 -0300

    atualizando rota home

--- functions/routes/webroutes/homeRoutes.js
+++ functions/routes/webroutes/homeRoutes.js
@@ -2,7 +2,7 @@ const homeRoutes = require('express').Router()
 
 
 homeRoutes.get('/index', (req, res)=>{
-      res.render('/index.hbs')
+      res.render('index.hbs')
 })
 
 module.exports = homeRoutes
