aee9dd7b9989f4cab2c692dc89ec664882fe378c
Homero <homerao@gmail.com>
2020-12-27 21:08:00 -0300

    adicionando rota de login

--- functions/routes/webroutes/homeRoutes.js
+++ functions/routes/webroutes/homeRoutes.js
@@ -2,12 +2,17 @@ const homeRoutes = require('express').Router()
 
 
 homeRoutes.get('/index', (req, res)=>{
-    const data = {title:'Home', userlogged:false}
+    const data = {title:'Home', userlogged:true}
       res.render('index.hbs', data)
 })
 
 homeRoutes.get('/', (req, res)=>{
   const data = {title:'Home', userlogged:false}
+    res.render('index.hbs', data)
+})
+
+homeRoutes.get('/login', (req, res)=>{
+  const data = {title:'login', userlogged:false}
     res.render('index.hbs', data)
 })
 module.exports = homeRoutes
