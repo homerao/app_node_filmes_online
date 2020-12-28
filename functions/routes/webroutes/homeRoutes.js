1a18bb5b15cfb2ad2f32914b6129afa17cdc0b9a
Homero <homerao@gmail.com>
2020-12-28 17:06:07 -0300

    ajustando dados nas páginas web

--- functions/routes/webroutes/homeRoutes.js
+++ functions/routes/webroutes/homeRoutes.js
@@ -2,7 +2,7 @@ const homeRoutes = require('express').Router()
 
 
 homeRoutes.get('/index', (req, res)=>{
-    const data = {title:'Home', userlogged:true}
+    const data = {title:'Index', userlogged:true}
     res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
       res.render('index.hbs', data)
 })
