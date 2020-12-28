f534b17579a9d8b7083fd6bef7a09b8a77ebced4
Homero <homerao@gmail.com>
2020-12-28 17:03:26 -0300

    adicionando cache nas rodas das páginas web

--- functions/database/mysql.js
+++ functions/database/mysql.js
@@ -1,13 +1,8 @@
 const Sequelize = require('sequelize')
-const proc = require('process')
-const firebase = require('firebase-admin')
-firebase.initializeApp()
-
-  
 
 
 
-module.exports  = new Sequelize(proc.env.DATABASE,proc.env.USER,proc.env.PASSWORD,{host:proc.env.HOST, dialect:proc.env.DIALECT})
+module.exports  = new Sequelize(process.env.DATABASE,process.env.USER,process.env.PASSWORD,{host:process.env.HOST, dialect:process.env.DIALECT})
 
 
 
--- functions/routes/webroutes/homeRoutes.js
+++ functions/routes/webroutes/homeRoutes.js
@@ -3,16 +3,19 @@ const homeRoutes = require('express').Router()
 
 homeRoutes.get('/index', (req, res)=>{
     const data = {title:'Home', userlogged:true}
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
       res.render('index.hbs', data)
 })
 
 homeRoutes.get('/', (req, res)=>{
   const data = {title:'Home', userlogged:false}
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
     res.render('index.hbs', data)
 })
 
 homeRoutes.get('/login', (req, res)=>{
   const data = {title:'login', userlogged:false}
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
     res.render('login.hbs', data)
 })
 module.exports = homeRoutes
