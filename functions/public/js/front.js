1258235922b95d3be92e1774fa29bced32f80817
Homero <homerao@gmail.com>
2020-12-29 20:23:30 -0300

    ajustando scripts front

--- functions/public/js/front.js
+++ functions/public/js/front.js
@@ -2,7 +2,7 @@ function carregou(){
     let data = document.getElementsByClassName('card-text');
     let newData= [];
   for(i=0; i< data.length; i++){
-   newData.push(data.item(i).innerText.substring(0,100) +'...');
-   data.item(i).innerText = newData[i];
+   newData.push(data.item(i).innerHTML.substring(0,100) +'...');
+   data.item(i).innerHTML = newData[i];
    alert("I am an alert box!");
   }
