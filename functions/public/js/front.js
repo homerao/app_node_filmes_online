84b429b1c963d307d302153e8a2db6445084ca45
Homero <homerao@gmail.com>
2020-12-29 20:21:02 -0300

    ajustando o javascript do front

--- functions/public/js/front.js
+++ functions/public/js/front.js
@@ -1,8 +1,8 @@
-function resumeSinopse(){
-    let tag = Document.querySelector('card-text')
-    let data = tag.innerText
-    let novaString = data.substring(0,100)+"..."
-    console.log(data)
-    tag.innerText = novaString
-    return true
-}
+function carregou(){
+    let data = document.getElementsByClassName('card-text');
+    let newData= [];
+  for(i=0; i< data.length; i++){
+   newData.push(data.item(i).innerText.substring(0,100) +'...');
+   data.item(i).innerText = newData[i];
+   alert("I am an alert box!");
+  }
