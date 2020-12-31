d654acdb99c862e7448b354104efc506a26c8a3f
Homero <homerao@gmail.com>
2020-12-31 13:45:41 -0300

    atualizando variaveis de ambiente

--- functions/database/mysql.js
+++ functions/database/mysql.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 
 
 
-module.exports  = new Sequelize(process.env.DATABASE,process.env.USER,process.env.PASSWORD,{host:process.env.HOST, dialect:process.env.DIALECT})
+module.exports  = new Sequelize(process.env.DATABASE,process.env.USER,process.env.PASSWORD,{host:process.env.MYSQL_HOST, dialect:process.env.DIALECT})
 
 
 
