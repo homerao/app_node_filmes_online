6112dff58a209c30a3d61fda0f0e2a82b2666e10
Homero <homerao@gmail.com>
2021-04-18 12:42:49 -0300

    atualizando package.json

--- functions/utils/s3bucket.js
+++ functions/utils/s3bucket.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const S3 = require('aws-sdk/clients/s3')
 const uuid = require('uuid')
 const s3 = new S3({credentials:{accessKeyId:process.env.AWS_ID, secretAccessKey:process.env.AWS_SECRET}})
@@ -15,22 +14,4 @@ const upload = async (path ="/", file, generatedName)=>{
 
 const  downlaod = async (bucket, file)=>{
 
-=======
-const S3 = require('aws-sdk/clients/s3')
-const uuid = require('uuid')
-const s3 = new S3({credentials:{accessKeyId:process.env.AWS_ID, secretAccessKey:process.env.AWS_SECRET}})
-const getFileType = (file) =>{
-  let fileName = file.split('.')[0]
-  let filetype = file.split('.')[1]
-  return filetype
-}
-const upload = async (path ="/", file, generatedName)=>{
-   s3.upload({Bucket:process.env.AWS_BUCKET+path, Key:generatedName,Body:file.buffer  }).on('httpUploadProgress'. )
-ffff
-}
-
-
-const  downlaod = async (bucket, file)=>{
-
->>>>>>> 575fabba31682a152198c274180edf97407e613c
 }
